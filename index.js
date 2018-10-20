7ca935ab41c82265a9c0589a732b965f2b2819b5
Galaxspheria <hunterghancock@gmail.com>
2018-10-19 22:31:42 -0400

    installed socket.io for client/server communication and wrote standard get request for interfacing with backend

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
+var https = require('https');
 var favicon = require('serve-favicon');
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -13,4 +18,31 @@ var port = process.env.PORT || 3000;
 
 http.listen(port, function () {
     console.log('listening on *:' + port);
+});
+
+function getURL(url) {
+    return (
+        new Promise((resolve, reject) => {
+            https.get(url, (resp) => {
+                let data = '';
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+                resp.on('end', () => {
+                    resolve(JSON.parse(data));
+                });
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+                reject();
+            });
+        })
+    )
+};
+
+io.on('connection', function (socket) {
+    console.log('user connected');
+    getURL('INSERTURL')
+    .then((result) => {
+        console.log(result);
+    })
 });
