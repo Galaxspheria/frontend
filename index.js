b929eccfc3000afdd618eea359303fbd4f414024
Galaxspheria <hunterghancock@gmail.com>
2018-10-19 22:16:42 -0400

    added favicon

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var favicon = require('serve-favicon');
+var path = require('path');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
